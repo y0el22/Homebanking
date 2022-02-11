dbe878a2704575e2fd0ded692d661ff0df7c4d90
y0el22 <yoeldts@hotmail.com>
2022-02-11 12:24:42 -0300

    new controller

---
+++ src/main/java/com/mindhub/homebanking/controllers/homeController.java
@@ -0,0 +1,18 @@
+package com.mindhub.homebanking.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class homeController {
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Spring Boot Example";
+    }
+
+
+
+}
